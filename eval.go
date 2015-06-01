d837a512761810eaf04fd5fe46062c57bf025ae6
Aymerick <aymerick@jehanne.org>
2015-06-01 10:07:49 +0200

    Removes debug log

--- eval.go
+++ eval.go
@@ -50,7 +50,6 @@ type EvalVisitor struct {
 
 // Instanciate a new evaluation visitor
 func NewEvalVisitor(tpl *Template, data interface{}, privData *DataFrame) *EvalVisitor {
-	log.Printf("privData: %q", privData)
 	frame := privData
 	if frame == nil {
 		frame = NewDataFrame()
