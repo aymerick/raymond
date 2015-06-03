4a91074aebdf1149a1082726cbc42bd082b31b81
Aymerick <aymerick@jehanne.org>
2015-06-03 21:29:51 +0200

    Adds examples for Template methods: Exec() MustExec() ExecWith() and PrintAST()

--- template_test.go
+++ template_test.go
@@ -1,6 +1,9 @@
 package raymond
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 var sourceBasic = `<div class="entry">
   <h1>{{title}}</h1>
@@ -37,4 +40,93 @@ func TestParse(t *testing.T) {
 	if str := tpl.PrintAST(); str != basicAST {
 		t.Errorf("Template parsing incorrect: %s", str)
 	}
+}
+
+func ExampleTemplate_Exec() {
+	source := "<h1>{{title}}</h1><p>{{body.content}}</p>"
+
+	ctx := map[string]interface{}{
+		"title": "foo",
+		"body":  map[string]string{"content": "bar"},
+	}
+
+	// parse template
+	tpl := MustParse(source)
+
+	// evaluate template with context
+	output, err := tpl.Exec(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf(output)
+	// Output: <h1>foo</h1><p>bar</p>
+}
+
+func ExampleTemplate_MustExec() {
+	source := "<h1>{{title}}</h1><p>{{body.content}}</p>"
+
+	ctx := map[string]interface{}{
+		"title": "foo",
+		"body":  map[string]string{"content": "bar"},
+	}
+
+	// parse template
+	tpl := MustParse(source)
+
+	// evaluate template with context
+	output := tpl.MustExec(ctx)
+
+	fmt.Printf(output)
+	// Output: <h1>foo</h1><p>bar</p>
+}
+
+func ExampleTemplate_ExecWith() {
+	source := "<h1>{{title}}</h1><p>{{#body}}{{content}} and {{@baz.bat}}{{/body}}</p>"
+
+	ctx := map[string]interface{}{
+		"title": "foo",
+		"body":  map[string]string{"content": "bar"},
+	}
+
+	// parse template
+	tpl := MustParse(source)
+
+	// computes private data frame
+	frame := NewDataFrame()
+	frame.Set("baz", map[string]string{"bat": "unicorns"})
+
+	// evaluate template
+	output, err := tpl.ExecWith(ctx, frame)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf(output)
+	// Output: <h1>foo</h1><p>bar and unicorns</p>
+}
+
+func ExampleTemplate_PrintAST() {
+	source := "<h1>{{title}}</h1><p>{{#body}}{{content}} and {{@baz.bat}}{{/body}}</p>"
+
+	// parse template
+	tpl := MustParse(source)
+
+	// print AST
+	output := tpl.PrintAST()
+
+	fmt.Printf(output)
+	// Output: CONTENT[ '<h1>' ]
+	// {{ PATH:title [] }}
+	// CONTENT[ '</h1><p>' ]
+	// BLOCK:
+	//   PATH:body []
+	//   PROGRAM:
+	//     {{     PATH:content []
+	//  }}
+	//     CONTENT[ ' and ' ]
+	//     {{     @PATH:baz/bat []
+	//  }}
+	//   CONTENT[ '</p>' ]
+	//
 }
