9cf76903b4fe87eda428598be498d84301db3889
Mark Bates <mark@markbates.com>
2016-12-27 10:31:12 -0500

    working on cleaning up tests to make them more readable

--- base_test.go
+++ base_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"regexp"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type Test struct {
@@ -19,6 +21,7 @@ type Test struct {
 func launchTests(t *testing.T, tests []Test) {
 	// NOTE: TestMustache() makes Parallel testing fail
 	// t.Parallel()
+	r := require.New(t)
 
 	for _, test := range tests {
 		var err error
@@ -26,65 +29,54 @@ func launchTests(t *testing.T, tests []Test) {
 
 		// parse template
 		tpl, err = Parse(test.input)
-		if err != nil {
-			t.Errorf("Test '%s' failed - Failed to parse template\ninput:\n\t'%s'\nerror:\n\t%s", test.name, test.input, err)
+		r.NoError(err)
+
+		if len(test.helpers) > 0 {
+			// register helpers
+			tpl.RegisterHelpers(test.helpers)
+		}
+
+		if len(test.partials) > 0 {
+			// register partials
+			tpl.RegisterPartials(test.partials)
+		}
+
+		// setup private data frame
+		var privData *DataFrame
+		if test.privData != nil {
+			privData = NewDataFrame()
+			for k, v := range test.privData {
+				privData.Set(k, v)
+			}
+		}
+
+		// render template
+		output, err := tpl.ExecWith(test.data, privData)
+		r.NoError(err)
+		// check output
+		var expectedArr []string
+		expectedArr, ok := test.output.([]string)
+		if ok {
+			match := false
+			for _, expectedStr := range expectedArr {
+				if expectedStr == output {
+					match = true
+					break
+				}
+			}
+
+			r.True(match)
 		} else {
-			if len(test.helpers) > 0 {
-				// register helpers
-				tpl.RegisterHelpers(test.helpers)
-			}
-
-			if len(test.partials) > 0 {
-				// register partials
-				tpl.RegisterPartials(test.partials)
-			}
-
-			// setup private data frame
-			var privData *DataFrame
-			if test.privData != nil {
-				privData = NewDataFrame()
-				for k, v := range test.privData {
-					privData.Set(k, v)
-				}
-			}
-
-			// render template
-			output, err := tpl.ExecWith(test.data, privData)
-			if err != nil {
-				t.Errorf("Test '%s' failed\ninput:\n\t'%s'\ndata:\n\t%s\nerror:\n\t%s\nAST:\n\t%s", test.name, test.input, Str(test.data), err, tpl.PrintAST())
-			} else {
-				// check output
-				var expectedArr []string
-				expectedArr, ok := test.output.([]string)
-				if ok {
-					match := false
-					for _, expectedStr := range expectedArr {
-						if expectedStr == output {
-							match = true
-							break
-						}
-					}
-
-					if !match {
-						t.Errorf("Test '%s' failed\ninput:\n\t'%s'\ndata:\n\t%s\npartials:\n\t%s\nexpected\n\t%q\ngot\n\t%q\nAST:\n%s", test.name, test.input, Str(test.data), Str(test.partials), expectedArr, output, tpl.PrintAST())
-					}
-				} else {
-					expectedStr, ok := test.output.(string)
-					if !ok {
-						panic(fmt.Errorf("Erroneous test output description: %q", test.output))
-					}
-
-					if expectedStr != output {
-						t.Errorf("Test '%s' failed\ninput:\n\t'%s'\ndata:\n\t%s\npartials:\n\t%s\nexpected\n\t%q\ngot\n\t%q\nAST:\n%s", test.name, test.input, Str(test.data), Str(test.partials), expectedStr, output, tpl.PrintAST())
-					}
-				}
-			}
+			expectedStr, ok := test.output.(string)
+			r.True(ok)
+			r.Equal(expectedStr, output)
 		}
 	}
 }
 
 func launchErrorTests(t *testing.T, tests []Test) {
 	t.Parallel()
+	r := require.New(t)
 
 	for _, test := range tests {
 		var err error
@@ -92,76 +84,65 @@ func launchErrorTests(t *testing.T, tests []Test) {
 
 		// parse template
 		tpl, err = Parse(test.input)
-		if err != nil {
-			t.Errorf("Test '%s' failed - Failed to parse template\ninput:\n\t'%s'\nerror:\n\t%s", test.name, test.input, err)
+		r.NoError(err)
+		if len(test.helpers) > 0 {
+			// register helpers
+			tpl.RegisterHelpers(test.helpers)
+		}
+
+		if len(test.partials) > 0 {
+			// register partials
+			tpl.RegisterPartials(test.partials)
+		}
+
+		// setup private data frame
+		var privData *DataFrame
+		if test.privData != nil {
+			privData := NewDataFrame()
+			for k, v := range test.privData {
+				privData.Set(k, v)
+			}
+		}
+
+		// render template
+		output, err := tpl.ExecWith(test.data, privData)
+		if err == nil {
+			t.Fatalf("Test '%s' failed - Error expected\ninput:\n\t'%s'\ngot\n\t%q\nAST:\n%q", test.name, test.input, output, tpl.PrintAST())
 		} else {
-			if len(test.helpers) > 0 {
-				// register helpers
-				tpl.RegisterHelpers(test.helpers)
-			}
+			var errMatch error
+			match := false
 
-			if len(test.partials) > 0 {
-				// register partials
-				tpl.RegisterPartials(test.partials)
-			}
+			// check output
+			var expectedArr []string
+			expectedArr, ok := test.output.([]string)
+			if ok {
+				if len(expectedArr) > 0 {
+					for _, expectedStr := range expectedArr {
+						match, errMatch = regexp.MatchString(regexp.QuoteMeta(expectedStr), fmt.Sprint(err))
+						r.NoError(errMatch)
 
-			// setup private data frame
-			var privData *DataFrame
-			if test.privData != nil {
-				privData := NewDataFrame()
-				for k, v := range test.privData {
-					privData.Set(k, v)
-				}
-			}
-
-			// render template
-			output, err := tpl.ExecWith(test.data, privData)
-			if err == nil {
-				t.Errorf("Test '%s' failed - Error expected\ninput:\n\t'%s'\ngot\n\t%q\nAST:\n%q", test.name, test.input, output, tpl.PrintAST())
-			} else {
-				var errMatch error
-				match := false
-
-				// check output
-				var expectedArr []string
-				expectedArr, ok := test.output.([]string)
-				if ok {
-					if len(expectedArr) > 0 {
-						for _, expectedStr := range expectedArr {
-							match, errMatch = regexp.MatchString(regexp.QuoteMeta(expectedStr), fmt.Sprint(err))
-							if errMatch != nil {
-								panic("Failed to match regexp")
-							}
-
-							if match {
-								break
-							}
+						if match {
+							break
 						}
-					} else {
-						// nothing to test
-						match = true
 					}
 				} else {
-					expectedStr, ok := test.output.(string)
-					if !ok {
-						panic(fmt.Errorf("Erroneous test output description: %q", test.output))
-					}
-
-					if expectedStr != "" {
-						match, errMatch = regexp.MatchString(regexp.QuoteMeta(expectedStr), fmt.Sprint(err))
-						if errMatch != nil {
-							panic("Failed to match regexp")
-						}
-					} else {
-						// nothing to test
-						match = true
-					}
+					// nothing to test
+					match = true
 				}
+			} else {
+				expectedStr, ok := test.output.(string)
+				r.True(ok)
 
-				if !match {
-					t.Errorf("Test '%s' failed - Incorrect error returned\ninput:\n\t'%s'\ndata:\n\t%s\nexpected\n\t%q\ngot\n\t%q", test.name, test.input, Str(test.data), test.output, err)
+				if expectedStr != "" {
+					match, errMatch = regexp.MatchString(regexp.QuoteMeta(expectedStr), fmt.Sprint(err))
+					r.NoError(errMatch)
+				} else {
+					// nothing to test
+					match = true
 				}
 			}
+
+			r.True(match)
 		}
 	}
 }
