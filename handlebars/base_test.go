599a3c2882120ac89bf7c11c7b45c98bda53cd4b
Mark Bates <mark@markbates.com>
2016-12-27 10:49:30 -0500

    another test cleaned up

--- handlebars/base_test.go
+++ handlebars/base_test.go
@@ -1,13 +1,13 @@
 package handlebars
 
 import (
-	"fmt"
 	"io/ioutil"
 	"path"
 	"strconv"
 	"testing"
 
 	"github.com/gobuffalo/ray"
+	"github.com/stretchr/testify/require"
 )
 
 // cf. https://github.com/aymerick/go-fuzz-tests/ray
@@ -27,6 +27,7 @@ type Test struct {
 
 func launchTests(t *testing.T, tests []Test) {
 	t.Parallel()
+	r := require.New(t)
 
 	for _, test := range tests {
 		var err error
@@ -34,67 +35,53 @@ func launchTests(t *testing.T, tests []Test) {
 
 		if dumpTpl {
 			filename := strconv.Itoa(dumpTplNb)
-			if err := ioutil.WriteFile(path.Join(".", "dump_tpl", filename), []byte(test.input), 0644); err != nil {
-				panic(err)
-			}
+			err := ioutil.WriteFile(path.Join(".", "dump_tpl", filename), []byte(test.input), 0644)
+			r.NoError(err)
 			dumpTplNb++
 		}
 
 		// parse template
 		tpl, err = ray.Parse(test.input)
-		if err != nil {
-			t.Errorf("Test '%s' failed - Failed to parse template\ninput:\n\t'%s'\nerror:\n\t%s", test.name, test.input, err)
+		r.NoError(err)
+		if len(test.helpers) > 0 {
+			// register helpers
+			tpl.RegisterHelpers(test.helpers)
+		}
+
+		if len(test.partials) > 0 {
+			// register partials
+			tpl.RegisterPartials(test.partials)
+		}
+
+		// setup private data frame
+		var privData *ray.DataFrame
+		if test.privData != nil {
+			privData = ray.NewDataFrame()
+			for k, v := range test.privData {
+				privData.Set(k, v)
+			}
+		}
+
+		// render template
+		output, err := tpl.ExecWith(test.data, privData)
+		r.NoError(err)
+		// check output
+		var expectedArr []string
+		expectedArr, ok := test.output.([]string)
+		if ok {
+			match := false
+			for _, expectedStr := range expectedArr {
+				if expectedStr == output {
+					match = true
+					break
+				}
+			}
+
+			r.True(match)
 		} else {
-			if len(test.helpers) > 0 {
-				// register helpers
-				tpl.RegisterHelpers(test.helpers)
-			}
-
-			if len(test.partials) > 0 {
-				// register partials
-				tpl.RegisterPartials(test.partials)
-			}
-
-			// setup private data frame
-			var privData *ray.DataFrame
-			if test.privData != nil {
-				privData = ray.NewDataFrame()
-				for k, v := range test.privData {
-					privData.Set(k, v)
-				}
-			}
-
-			// render template
-			output, err := tpl.ExecWith(test.data, privData)
-			if err != nil {
-				t.Errorf("Test '%s' failed\ninput:\n\t'%s'\ndata:\n\t%s\nerror:\n\t%s\nAST:\n\t%s", test.name, test.input, ray.Str(test.data), err, tpl.PrintAST())
-			} else {
-				// check output
-				var expectedArr []string
-				expectedArr, ok := test.output.([]string)
-				if ok {
-					match := false
-					for _, expectedStr := range expectedArr {
-						if expectedStr == output {
-							match = true
-							break
-						}
-					}
-
-					if !match {
-						t.Errorf("Test '%s' failed\ninput:\n\t'%s'\ndata:\n\t%s\npartials:\n\t%s\nexpected\n\t%q\ngot\n\t%q\nAST:\n%s", test.name, test.input, ray.Str(test.data), ray.Str(test.partials), expectedArr, output, tpl.PrintAST())
-					}
-				} else {
-					expectedStr, ok := test.output.(string)
-					if !ok {
-						panic(fmt.Errorf("Erroneous test output description: %q", test.output))
-					}
-
-					if expectedStr != output {
-						t.Errorf("Test '%s' failed\ninput:\n\t'%s'\ndata:\n\t%s\npartials:\n\t%s\nexpected\n\t%q\ngot\n\t%q\nAST:\n%s", test.name, test.input, ray.Str(test.data), ray.Str(test.partials), expectedStr, output, tpl.PrintAST())
-					}
-				}
-			}
+			expectedStr, ok := test.output.(string)
+			r.True(ok)
+			r.Equal(expectedStr, output)
 		}
 	}
 }
